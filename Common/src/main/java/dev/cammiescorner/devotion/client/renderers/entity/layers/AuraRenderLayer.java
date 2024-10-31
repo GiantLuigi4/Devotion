9bff54c477dd66bf3907c7e81987f1e35571d357
CammiePone <inazuma261@gmail.com>
2024-10-31 12:00:31 -0700

    added if block to render layer

--- Common/src/main/java/dev/cammiescorner/devotion/client/renderers/entity/layers/AuraRenderLayer.java
+++ Common/src/main/java/dev/cammiescorner/devotion/client/renderers/entity/layers/AuraRenderLayer.java
@@ -24,20 +24,22 @@ public class AuraRenderLayer<T extends LivingEntity, M extends EntityModel<T>> e
 	public void render(PoseStack poseStack, MultiBufferSource bufferSource, int light, T entity, float limbSwing, float limbSwingAmount, float partialTick, float ageInTicks, float netHeadYaw, float headPitch) {
 		float aura = 1f;
 		float scale = 1f;
-		EntityDimensions dimensions = entity.getDimensions(entity.getPose());
 
-		AuraVertexBufferSource auraBufferSource = new AuraVertexBufferSource(bufferSource);
-		auraBufferSource.setColor(255, 255, 255, (int) (aura * 255));
+		if(aura > 0f) {
+			EntityDimensions dimensions = entity.getDimensions(entity.getPose());
+			AuraVertexBufferSource auraBufferSource = new AuraVertexBufferSource(bufferSource);
+			auraBufferSource.setColor(255, 255, 255, (int) (aura * 255));
 
-		poseStack.pushPose();
-		poseStack.scale(scale, scale, scale);
+			poseStack.pushPose();
+			poseStack.scale(scale, scale, scale);
 
-		for(RenderLayer<T, M> renderer : otherFeatureRenderers)
-			renderer.render(poseStack, auraBufferSource, light, entity, limbSwing, limbSwingAmount, partialTick, ageInTicks, netHeadYaw, headPitch);
+			for(RenderLayer<T, M> renderer : otherFeatureRenderers)
+				renderer.render(poseStack, auraBufferSource, light, entity, limbSwing, limbSwingAmount, partialTick, ageInTicks, netHeadYaw, headPitch);
 
-		poseStack.translate(0D, -((dimensions.height() * scale) * 0.5D - dimensions.height() * 0.5D), 0D);
-		getParentModel().renderToBuffer(poseStack, auraBufferSource.getBuffer(getTextureLocation(entity)), light, OverlayTexture.NO_OVERLAY, 0xffffffff);
+			poseStack.translate(0D, -((dimensions.height() * scale) * 0.5D - dimensions.height() * 0.5D), 0D);
+			getParentModel().renderToBuffer(poseStack, auraBufferSource.getBuffer(getTextureLocation(entity)), light, OverlayTexture.NO_OVERLAY, 0xffffffff);
 
-		poseStack.popPose();
+			poseStack.popPose();
+		}
 	}
 }
