5af9545b05ea453543650bace417511de88c3c90
CammiePone <inazuma261@gmail.com>
2024-11-10 13:00:35 -0800

    rewrite riddledata stream codec

--- Common/src/main/java/dev/cammiescorner/devotion/api/research/RiddleData.java
+++ Common/src/main/java/dev/cammiescorner/devotion/api/research/RiddleData.java
@@ -5,16 +5,35 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.cammiescorner.devotion.api.spells.AuraType;
 import net.minecraft.network.RegistryFriendlyByteBuf;
-import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record RiddleData(List<Pair<AuraType, Integer>> riddles) {
 	public static final Codec<RiddleData> CODEC = RecordCodecBuilder.create(riddleDataInstance -> riddleDataInstance.group(
 		Codec.compoundList(AuraType.CODEC, Codec.INT).fieldOf("riddles").forGetter(RiddleData::riddles)
 	).apply(riddleDataInstance, RiddleData::new));
-	public static final StreamCodec<RegistryFriendlyByteBuf, RiddleData> STREAM_CODEC = ByteBufCodecs.fromCodecWithRegistriesTrusted(CODEC);
+	public static final StreamCodec<RegistryFriendlyByteBuf, RiddleData> STREAM_CODEC = StreamCodec.of((buffer, value) -> {
+		List<Pair<AuraType, Integer>> riddles = value.riddles();
+		int riddleCount = riddles.size();
+
+		buffer.writeVarInt(riddleCount);
+
+		for(int i = 0; i < riddleCount; i++) {
+			Pair<AuraType, Integer> pair = riddles.get(i);
+			buffer.writeEnum(pair.getFirst());
+			buffer.writeVarInt(pair.getSecond());
+		}
+	}, buffer -> {
+		List<Pair<AuraType, Integer>> riddles = new ArrayList<>();
+		int riddleCount = buffer.readVarInt();
+
+		for(int i = 0; i < riddleCount; i++)
+			riddles.add(Pair.of(buffer.readEnum(AuraType.class), buffer.readVarInt()));
+
+		return new RiddleData(riddles);
+	});
 
 	public Pair<AuraType, Integer> getRiddle(int index) {
 		return index < riddles.size() ? riddles.get(index) : null;
